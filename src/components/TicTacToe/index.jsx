b6917d1b4631ab2307ba487caa1e159c0433d804
Parth Patel <200425734@student.georgianc.on.ca>
2020-10-22 10:05:05 -0400

    firstcommit

--- src/components/TicTacToe/index.jsx
+++ src/components/TicTacToe/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from '../shared/Header'
 const TicTacToe = () => {
   return (
     <>
-      
+      <Header title = "Lets do this Thang"/>
     </>
   );
 }
